a174a5a566243e9dc7e30243a3b6b44f29a1d30a
herianwar78 <herianwar78@student.esaunggul.ac.id>
2022-10-15 14:34:04 +0700

    tugas sesi 4

---
+++ BP_sesi4/variabel/src.py
@@ -0,0 +1,27 @@
+#variabel
+a = "Ahmad Khoirudin"
+def func():
+	a = "any"
+	print :("Selamat " + a)
+func()
+print (a)
+
+#definisi
+def tambah():
+	a = 20
+	b = 25
+	c = a + b
+	print (c)
+
+tambah ()
+
+#definisi parameter
+def data (nama,nim):
+	print(f"nama saya {nama} dan {nim}")
+data("Ahmad","20210801109")
+
+#contoh
+def total(sisi):
+	return sisi*sisi
+def segitiga(alas,tinggi):
+	return 0.5*alas*tinggi
